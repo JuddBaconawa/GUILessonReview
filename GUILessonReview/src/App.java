e31f32f261b64eee7fe2c0a9256f72799a2b00f0
Judd Baconawa <75963954+JuddBaconawa@users.noreply.github.com>
2025-03-03 16:35:33 -0500

    New java file created on VisualStudio code
    Main java file created - App.java: comments added + default settings implemented + README file created.

---
+++ GUILessonReview/src/App.java
@@ -0,0 +1,17 @@
+//packages
+
+
+//imports
+
+
+//main lesson App
+public class App {
+
+    //main method
+    public static void main(String[] args) throws Exception {
+        
+        
+
+
+    }
+}
